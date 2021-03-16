914cf9c3f50c7af103d46b7d35caf1e56e59bed1
charlie-millie <charlie@millie.town>
2021-03-16 18:47:19 +0900

    - todo 작업진행#1

--- src/script/Todo.js
+++ src/script/Todo.js
@@ -1,4 +1,5 @@
 import Storage from "./Storage";
+import DOM from "./DOM";
 
 export default class Todo {
     constructor(target) {
@@ -7,15 +8,81 @@ export default class Todo {
 
         console.log("Todo: ", this);
         this.init();
+        this.initList();
+
+        // Date.now()
     }
 
     init() {
+        this.todoListBox = DOM.createElement("div", {
+            attrs: {
+                class: "todoListBox"
+            },
+            parent: this.target
+        });
+
+        this.todoInput = DOM.createElement("input", {
+            attrs: {
+                class: "todoInput"
+            },
+            parent: this.todoListBox
+        });
+
+      /*  this.deleteButton = DOM.createElement("button", {
+            attrs: {
+                class: "deleteButton"
+            },
+            text: "삭제",
+            parent: this.todoListBox
+        });*/
+
+
+        this.addButton = DOM.createElement("button", {
+            attrs: {
+                class: "addButton"
+            },
+            text: "추가",
+            parent: this.todoListBox
+        });
+        this.addButton.addEventListener("click", this.addToDo);
+
+        this.deleteAllButton = DOM.createElement("button", {
+            attrs: {
+                class: "deleteAllButton"
+            },
+            text: "전체삭제",
+            parent: this.target
+        });
+    }
+
+    initList() {
+        if (this.storage.length > 0) {
+            const emptyList = DOM.createElement("div", {
+                attrs: {
+                    class: "emptyList"
+                },
+                text: "등록된 ToDo 아이템이 없습니다.",
+                parent: this.todoListBox
+            });
+        } else {
+            this.storage.forEach(item => {
+
+            });
+        }
 
     }
 
+    addToDo(e) {
+        console.log(this.todoInput.value);
 
 
-    renderList() {
+    }
+
+    delToDo() {
+
+    }
+
+    deleteAll() {
 
     }
 
